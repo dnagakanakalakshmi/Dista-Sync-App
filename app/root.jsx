35d38d3e7a10dc168fadc116999b952e26cc99ae
dnagakanakalakshmi <kanakalakshmi.dwarampudi@distacart.com>
2025-10-22 11:26:42 +0530

    Modified monitoring web vitals

--- app/root.jsx
+++ app/root.jsx
@@ -20,6 +20,7 @@ export default function App() {
     <html>
       <head>
         <meta charSet="utf-8" />
+        <meta name="shopify-debug" content="web-vitals" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
         <link
